321dd1719c61ff61e7e60c19302f163975f5f9e9
InternetCafeCoin <48309742+InternetCafeCoin@users.noreply.github.com>
2019-03-25 14:47:12 +0900

    Update icc-update-v1.0.sh

--- icc-update-v1.0.sh
+++ icc-update-v1.0.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 echo -e "\n\nupdate iccd ...\n\n"
@@ -5,18 +6,34 @@ cd /root/icc
 ./icc-cli stop
 sleep 10
 
-wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-linux-v1.0.zip
-chmod -R 755 /root/icc-linux-v1.0.zip
-unzip -o icc-linux-v1.0.zip
+rm /root/icc/iccd
+rm /root/icc/icc-cli
+
+sleep 1 
+
+version=`lsb_release -r | awk '{print $2}
+echo "ubuntu version : "\n
+echo $version
+
+if [ $version = "16.04" ]; then
+    echo "setup icc for ubuntu 16.04\n"
+    wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-ubuntu16.04-v1.0.zip
+    chmod -R 755 /root/icc-ubuntu16.04-v1.0.zip
+    unzip -o /root/icc/icc-ubuntu16.04-v1.0.zip        
+else
+    echo "setup icc for ubuntu 18.04\n"
+    wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-ubuntu18.04-v1.0.zip
+    chmod -R 755 /root/icc-ubuntu18.04-v1.0.zip
+    unzip -o /root/icc/icc-ubuntu18.04-v1.0.zip
+fi
+
 sleep 5
 
-rm /root/iccd
-rm /root/icc-cli
-rm /root/icc-linux-v1.0.zip
+rm /root/icc/icc-*-v1.0.zip
 
 chmod -R 755 /root/icc/*
 
 echo -e "\n\nlaunch iccd ...\n\n"
 ./iccd -reindex
 
-echo "ICC Masternode updated"
+echo "ICC Masternode Updated"
