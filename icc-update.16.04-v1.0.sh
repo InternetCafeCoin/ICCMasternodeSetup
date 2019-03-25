22ccfb34a1659b146e1c266d9fd38d9e5c24287d
InternetCafeCoin <48309742+InternetCafeCoin@users.noreply.github.com>
2019-03-25 13:24:35 +0900

    Create icc-update.16.04-v1.0.sh

---
+++ icc-update.16.04-v1.0.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+echo -e "\n\nupdate iccd ...\n\n"
+cd /root/icc
+./icc-cli stop
+sleep 10
+
+rm /root/iccd
+rm /root/icc-cli
+
+wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-ubuntu16.04-v1.0.zip
+chmod -R 755 /root/icc/icc-ubuntu16.04-v1.0.zip
+unzip -o /root/icc/icc-ubuntu16.04-v1.0.zip
+sleep 5
+
+rm /root/icc/icc-ubuntu16.04-v1.0.zip
+
+chmod -R 755 /root/icc/*
+
+echo -e "\n\nlaunch iccd ...\n\n"
+./iccd -reindex
+
+echo "ICC Masternode updated"
