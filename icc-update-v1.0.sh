fae0aed4d5c78924ae25069d4944da06fdf9b4e3
InternetCafeCoin <48309742+InternetCafeCoin@users.noreply.github.com>
2019-06-18 12:49:53 +0900

    Update icc-update-v1.0.sh

--- icc-update-v1.0.sh
+++ icc-update-v1.0.sh
@@ -15,17 +15,10 @@ version=`lsb_release -r | awk '{print $2}'`
 echo "ubuntu version : "\n
 echo $version
 
-if [ $version = "16.04" ]; then
-    echo "setup icc for ubuntu 16.04\n"
-    wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-ubuntu16.04-v1.0.zip
-    chmod -R 755 /root/icc-ubuntu16.04-v1.0.zip
-    unzip -o /root/icc/icc-ubuntu16.04-v1.0.zip        
-else
-    echo "setup icc for ubuntu 18.04\n"
-    wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-ubuntu18.04-v1.0.zip
-    chmod -R 755 /root/icc-ubuntu18.04-v1.0.zip
-    unzip -o /root/icc/icc-ubuntu18.04-v1.0.zip
-fi
+
+echo "setup icc for ubuntu $version\n"
+wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/icc-cli
+wget https://github.com/InternetCafeCoin/ICC-CORE/releases/download/v1.0/iccd
 
 sleep 5
 
